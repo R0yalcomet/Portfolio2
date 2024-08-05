c7e28ca001e02c3820f3c0011f91ee592dece772
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-08-05 15:10:27 -0500

    "create Contact page structure"

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <section id="contact">
+            <h1>Contact Me</h1>
+            <h2>Preferred Contact</h2>
+            <h3>Email: <strong>rltisdale@outlook.com</strong></h3>
+            <h2>Other Info</h2>
+            <h3>Alternate Email: <strong>rltisdale@mail.com</strong></h3>
+            <h3>Phone: <strong>(913) 775-2686</strong></h3>
+            <h3>Github: <strong>github.com/R0yalcomet</strong></h3>
+        </section>
+    )
+}
+
+export default Contact;
