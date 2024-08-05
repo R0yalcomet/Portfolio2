26e8b4b60549548bf8a45578b24b775c9576e110
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-08-05 14:49:19 -0500

    "create home page structure"

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import AppLayout from "./AppLayout";
+import Home from "../Components/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AppLayout/>}>
-        
+          <Route path="" element={<Home/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <section id="banner">
+                <div>
+                    <h1>Roy Tisdale</h1>
+                    <h3>Front-End Engineer</h3>
+                </div>
+                <div>
+                    <h2>About Me</h2>
+                    <p>Example text blah blah blah...</p>
+                </div>
+            </section>
+            <section id="featured">
+                <h2>Featured Project</h2>
+                <div>
+                    <h3>Project Name Here</h3>
+                    <p>Project description here blah blah blah...</p>
+                    <p>Tools Used: blah, blah, blah</p>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Home;
