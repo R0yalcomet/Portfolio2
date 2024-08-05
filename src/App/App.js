23064a2183dafd5dcda3136212b3756ffc0d5989
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-08-05 12:48:33 -0500

    "configure react and react router"

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
