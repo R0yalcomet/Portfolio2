d75e84084cfa57acf688212925e8d57c6da5782d
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-08-05 14:10:03 -0500

    "create AppLayout and header"

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
+import AppLayout from "./AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<AppLayout/>}>
         
+        </Route>
       </Routes>
     </BrowserRouter>
   );
