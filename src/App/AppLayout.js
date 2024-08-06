ca4a8f69e69c24cbdade4677450ace07feed18ea
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-08-06 16:50:33 -0500

    "add simple navigation links"

--- src/App/AppLayout.js
+++ src/App/AppLayout.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const AppLayout = () => {
     return (
         <div>
             <header>
                 <h1>Portfolio - Roy Tisdale</h1>
+                <nav>
+                    <Link to={"/"}><h3>Home</h3></Link>
+                    <Link to={"/projects"}><h3>Projects</h3></Link>
+                    <Link to={"/contact"}><h3>Contact</h3></Link>
+                </nav>
             </header>
             <Outlet/>
         </div>
