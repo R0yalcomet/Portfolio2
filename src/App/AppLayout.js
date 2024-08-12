9ebebbe1ef3b4c161e7d527c188654d3817e3e1a
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-08-12 17:35:57 -0500

    "background styling, scroll disabled temporarily"

--- src/App/AppLayout.js
+++ src/App/AppLayout.js
@@ -3,7 +3,7 @@ import { Outlet, Link } from "react-router-dom";
 
 const AppLayout = () => {
     return (
-        <html>
+        <div>
             <header>
                 <h1>Portfolio - Roy Tisdale</h1>
                 <nav>
@@ -13,7 +13,7 @@ const AppLayout = () => {
                 </nav>
             </header>
             <Outlet/>
-        </html>
+        </div>
     )
 }
 
