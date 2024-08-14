1ed1f3957e131013e3389c87a9c9447620d46110
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-08-14 16:12:08 -0500

    "path-specific styling for nav buttons"

--- src/App/AppLayout.js
+++ src/App/AppLayout.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 const AppLayout = () => {
+    let location = useLocation();
+
     return (
         <div>
             <header>
                 <h1>Portfolio - Roy Tisdale</h1>
                 <nav>
-                    <Link className="navButton" to={"/"}><h3>Home</h3></Link>
-                    <Link className="navButton" to={"/projects"}><h3>Projects</h3></Link>
-                    <Link className="navButton" to={"/contact"}><h3>Contact</h3></Link>
+                    <Link className={location.pathname === "/" ? "currentPage navButton" : "navButton"} to={"/"}><h3>Home</h3></Link>
+                    <Link className={location.pathname === "/projects" ? "currentPage navButton" : "navButton"} to={"/projects"}><h3>Projects</h3></Link>
+                    <Link className={location.pathname === "/contact" ? "currentPage navButton" : "navButton"} to={"/contact"}><h3>Contact</h3></Link>
                 </nav>
             </header>
             <Outlet/>
