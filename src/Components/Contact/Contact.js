b732952767d81c30e14a4bad5f62aa53dc73a723
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-08-14 14:45:37 -0500

    "Contact page styling"

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,13 +4,16 @@ const Contact = () => {
     return (
         <div className="contentContainer">
             <section id="contact">
-                <h1>Contact Me</h1>
-                <h2>Preferred Contact</h2>
-                <h3>Email: <strong>rltisdale@outlook.com</strong></h3>
-                <h2>Other Info</h2>
-                <h3>Alternate Email: <strong>rltisdale@mail.com</strong></h3>
-                <h3>Phone: <strong>(913) 775-2686</strong></h3>
-                <h3>Github: <strong>github.com/R0yalcomet</strong></h3>
+                <div id="contactPhoto"></div>
+                <div id="contactBody">
+                    <h1>Contact Me</h1><br/>
+                    <h2>Preferred Contact</h2>
+                    <h3>Email: <strong>rltisdale@outlook.com</strong></h3><br/>
+                    <h2>Other Info</h2>
+                    <h3>Alternate Email: <strong>rltisdale@mail.com</strong></h3>
+                    <h3>Phone: <strong>(913) 775-2686</strong></h3>
+                    <h3>Github: <strong>github.com/R0yalcomet</strong></h3>
+                </div>
             </section>
         </div>
     )
