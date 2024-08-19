543d59d7cd7eb4cefbb19f5fb7e4c1152d1f625b
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-08-19 17:08:52 -0500

    "update reddit app info"

--- src/Components/Projects/projectsData.js
+++ src/Components/Projects/projectsData.js
@@ -21,14 +21,14 @@ import RedditApp from "./projectImages/RedditApp.jpg";
 const projectsData = [
     {
         id: "redditapp",
-        name: "Reddit App",
+        name: "Reddit-Lite",
         image: RedditApp,
         summary: "A web app that fetches data from Reddit.",
         goal: "The goal of this project was to create a web app that allows the user to view and browse posts by fetching data from Reddit's unofficial JSON API.",
         features: "The user is able to filter the displayed posts using a search term or selecting a subreddit from the list. The user may also click a chosen post to view the full post and the comments under the post.",
-        commentary: "The biggest challenge I encountered while working on this project was in relation to the API. I intended for the user to be able to use both the search function and subreddit filters concurrently, but one would always overwrite the other. While I believe my code for the API interactions should be able to make the proper requests for data matching both the search and filter constraints, the API continues to return data that only matches one of these constraints. Unfortunately I have yet to find a fix for this issue and the user is limited to choosing between the search function or the subreddit filters, but not both.",
-        tools: "List of technologies used ex: javascript, React, etc...",
-        todo: "to do"
+        commentary: "During the development of this project I encountered a challenge in relation to the API. I intended for the user to be able to use both the search function and subreddit filters concurrently, but one would always overwrite the other. While I believe my code for the API interactions should be able to make the proper requests for data matching both the search and filter constraints, the API continues to return data that only matches one of these constraints. Unfortunately I have yet to find a fix for this issue and the user is limited to choosing between the search function or the subreddit filters, but not both.",
+        tools: "Javascript, CSS, React, React-Router, Redux, API",
+        todo: ["improve styling", "hide empty/deleted comments", "screen-size responsive styling", "fix filter vs search API conflict"]
     },
     {
         id: "projectname2",
