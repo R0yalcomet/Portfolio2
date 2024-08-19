895c44c234e43b3a9018bbc39e924390d3912a46
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-08-19 15:45:35 -0500

    "change how project data is stored and displayed"

--- src/Components/Projects/ProjectSingleView.js
+++ src/Components/Projects/ProjectSingleView.js
@@ -13,8 +13,15 @@ const ProjectSingleView = ({project = null, featured = false}) => {
         <section id="singleView">
             {featured ? <><h2>Featured Project</h2><h3>{project.name}</h3></> : <h2>{project.name}</h2>}
             <img src={project.image}/>
-            <p>{project.description}</p>
             <p>{project.tools}</p>
+            <br/>
+            <p>{project.goal}</p>
+            <br/>
+            <p>{project.features}</p>
+            <br/>
+            <p>{project.commentary}</p>
+            <br/>
+            <p>{project.todo}</p>
         </section>
     )
 }
--- src/Components/Projects/projectsData.js
+++ src/Components/Projects/projectsData.js
@@ -1,38 +1,67 @@
 import React from "react";
 import RedditApp from "./projectImages/RedditApp.jpg";
 
+/* 
+    new project template:
+
+    {
+        id: "projectname",
+        name: "Project Name",
+        image: null,
+        summary: "1-2 sentence description",
+        goal: "~3 sentences about the intended end result",
+        features: "what does the project do?",
+        commentary: "additional info about particular challenges or the development of the project",
+        tools: "list of technologies used ex: javascript, React, etc...",
+        todo: "fixes, adjustments, or extra features not yet present in the project",
+    }
+
+*/
+
 const projectsData = [
     {
         id: "redditapp",
         name: "Reddit App",
         image: RedditApp,
         summary: "A web app that fetches data from Reddit.",
-        description: <>
-            The goal of this project was to create a web app that allows the user to view and browse posts by fetching data from Reddit's unofficial JSON API.
-            <br/><br/>
-            The user is able to filter the displayed posts using a search term or selecting a subreddit from the list. The user may also click a chosen post to view the full post and the comments under the post.
-            <br/><br/> 
-            The biggest challenge I encountered while working on this project was in relation to the API. I intended for the user to be able to use both the search function and subreddit filters concurrently, but one would always overwrite the other. While I believe my code for the API interactions should be able to make the proper requests for data matching both the search and filter constraints, the API continues to return data that only matches one of these constraints. Unfortunately I have yet to find a fix for this issue and the user is limited to choosing between the search function or the subreddit filters, but not both.
-            <br/><br/>
-
-        </>,
-        tools: "List of technologies used ex: javascript, React, etc..."
+        goal: "The goal of this project was to create a web app that allows the user to view and browse posts by fetching data from Reddit's unofficial JSON API.",
+        features: "The user is able to filter the displayed posts using a search term or selecting a subreddit from the list. The user may also click a chosen post to view the full post and the comments under the post.",
+        commentary: "The biggest challenge I encountered while working on this project was in relation to the API. I intended for the user to be able to use both the search function and subreddit filters concurrently, but one would always overwrite the other. While I believe my code for the API interactions should be able to make the proper requests for data matching both the search and filter constraints, the API continues to return data that only matches one of these constraints. Unfortunately I have yet to find a fix for this issue and the user is limited to choosing between the search function or the subreddit filters, but not both.",
+        tools: "List of technologies used ex: javascript, React, etc...",
+        todo: "to do"
     },
     {
         id: "projectname2",
         name: "Project Name 2",
         image: null,
-        summary: "Brief description of what the project is.",
-        description: "More detailed description of the project and its making.",
-        tools: "List of technologies used ex: javascript, React, etc..."
+        summary: "",
+        goal: "",
+        features: "",
+        commentary: "",
+        tools: "List of technologies used ex: javascript, React, etc...",
+        todo: "",
     },
     {
         id: "projectname3",
         name: "Project Name 3",
         image: null,
-        summary: "Brief description of what the project is.",
-        description: "More detailed description of the project and its making.",
-        tools: "List of technologies used ex: javascript, React, etc..."
+        summary: "",
+        goal: "",
+        features: "",
+        commentary: "",
+        tools: "List of technologies used ex: javascript, React, etc...",
+        todo: "",
+    },
+    {
+        id: "projectname4",
+        name: "Project Name 4",
+        image: null,
+        summary: "",
+        goal: "",
+        features: "",
+        commentary: "",
+        tools: "List of technologies used ex: javascript, React, etc...",
+        todo: "",
     }
 ];
 
